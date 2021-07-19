d3ac6bb630494be08ba398b8778d473bb912c923
David Oliveira <dol.jpo@hotmail.com>
2021-07-19 00:14:12 -0300

    refactoring

--- border-previewer.js
+++ border-previewer.js
@@ -1,33 +1,31 @@
-var css = "";
-
 function apply() {
     let topLeftRadius = getTopLeftRadius();
     let bottomLeftRadius = getBottomLeftRadius();
     let topRightRadius = getTopRightRadius();
     let bottomRightRadius = getBottomRightRadius();
 
-    css = `${topLeftRadius}; ${bottomLeftRadius}; ${topRightRadius}; ${bottomRightRadius}`;
+    let css = `${topLeftRadius} ${bottomLeftRadius} ${topRightRadius} ${bottomRightRadius}`;
     applyBorderRadius(css);
 }
 
 function getTopLeftRadius() {
     let radius = document.getElementById("top-left-border").value;
-    return `border-top-left-radius: ${radius}px;`;
+    return !radius ? '' : `border-top-left-radius: ${radius}px;`;
 }
 
 function getBottomLeftRadius() {
     let radius = document.getElementById("bottom-left-border").value;
-    return `border-bottom-left-radius: ${radius}px;`;
+    return !radius ? '' : `border-bottom-left-radius: ${radius}px;`;
 }
 
 function getTopRightRadius() {
     let radius = document.getElementById("top-right-border").value;
-    return `border-top-right-radius: ${radius}px;`;
+    return !radius ? '' : `border-top-right-radius: ${radius}px;`;
 }
 
 function getBottomRightRadius() {
     let radius = document.getElementById("bottom-right-border").value;
-    return `border-bottom-right-radius: ${radius}px;`;
+    return !radius ? '' : `border-bottom-right-radius: ${radius}px;`;
 }
 
 function reset() {
@@ -42,12 +40,4 @@ function reset() {
 function applyBorderRadius(borderRadius) {
     document.getElementById("box").style = borderRadius;
     document.getElementById("css-generated").value = borderRadius;
-
-    if (!borderRadius) {
-        document.getElementById("div-css").classList.remove("display-block");
-        document.getElementById("div-css").classList.add("display-none");
-    } else {
-        document.getElementById("div-css").classList.remove("display-none");        
-        document.getElementById("div-css").classList.add("display-block");
-    }
 }
