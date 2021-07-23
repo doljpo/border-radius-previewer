5c050be0edba24b55a15d3f98d4785f39e76585c
David Oliveira <dol.jpo@hotmail.com>
2021-07-22 23:06:00 -0300

    better CSS code generated; added google fonts

--- border-previewer.js
+++ border-previewer.js
@@ -4,28 +4,45 @@ function apply() {
     let topRightRadius = getTopRightRadius();
     let bottomRightRadius = getBottomRightRadius();
 
-    let css = `${topLeftRadius} ${bottomLeftRadius} ${topRightRadius} ${bottomRightRadius}`;
+    let css = '';
+    if (topLeftRadius == bottomLeftRadius && 
+        topLeftRadius == topRightRadius && 
+        topLeftRadius == bottomLeftRadius) {
+            css = `border-radius: ${topLeftRadius}px`;
+    } else {
+        css = generateBorderRadiusCss(topLeftRadius, bottomLeftRadius, topRightRadius, bottomRightRadius);
+    }
+
     applyBorderRadius(css);
 }
 
 function getTopLeftRadius() {
     let radius = document.getElementById("top-left-border").value;
-    return !radius ? '' : `border-top-left-radius: ${radius}px;`;
+    return !radius ? 0 : radius;
 }
 
 function getBottomLeftRadius() {
     let radius = document.getElementById("bottom-left-border").value;
-    return !radius ? '' : `border-bottom-left-radius: ${radius}px;`;
+    return !radius ? 0 : radius;
 }
 
 function getTopRightRadius() {
     let radius = document.getElementById("top-right-border").value;
-    return !radius ? '' : `border-top-right-radius: ${radius}px;`;
+    return !radius ? 0 : radius;
 }
 
 function getBottomRightRadius() {
     let radius = document.getElementById("bottom-right-border").value;
-    return !radius ? '' : `border-bottom-right-radius: ${radius}px;`;
+    return !radius ? 0 : radius;
+}
+
+function generateBorderRadiusCss(topLeftRadius, bottomLeftRadius, topRightRadius, bottomRightRadius) {
+    let topLeftRadiusCss = topLeftRadius == 0 ? '' : `border-top-left-radius: ${topLeftRadius}px;`;
+    let bottomLeftRadiusCss = bottomLeftRadius == 0 ? '' : `border-bottom-left-radius: ${bottomLeftRadius}px;`;
+    let topRightRadiusCss = topRightRadius == 0 ? '' : `border-top-right-radius: ${topRightRadius}px;`;
+    let bottomRightRadiusCss = bottomRightRadius == 0 ? '' : `border-bottom-right-radius: ${bottomRightRadius}px;`;
+
+    return `${topLeftRadiusCss} ${bottomLeftRadiusCss} ${topRightRadiusCss} ${bottomRightRadiusCss}`;
 }
 
 function reset() {
